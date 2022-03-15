b41c05e251c493872cd1fb75c6d45f01357af546
hongsk <hongsk@host.docker.internal>
2022-03-15 17:20:15 +0900

    [CHORE] first commit

---
+++ src/main/java/com/nanuri/work/NanuriWorkApplication.java
@@ -0,0 +1,13 @@
+package com.nanuri.work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NanuriWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NanuriWorkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nanuri/work/NanuriWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nanuri.work;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NanuriWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
