2dad53cb6a89e293c05d6c07072afb211b1d2cd4
hongsk <hongsk@host.docker.internal>
2022-03-15 17:58:04 +0900

    [CHORE] 환경설정

--- src/main/java/com/nanuri/work/NanuriWorkApplication.java
+++ src/main/java/com/nanuri/work/NanuriWorkApplication.java
@@ -1,10 +1,22 @@
 package com.nanuri.work;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
 @SpringBootApplication
 public class NanuriWorkApplication {
+	
+	@PostConstruct
+    public void config() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(NanuriWorkApplication.class, args);
