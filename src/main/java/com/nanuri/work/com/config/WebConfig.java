0629f85fbd03ac7577b02faa0654acfba0e80b25
hongsk <hongsk@DESKTOP-67E35LK>
2022-03-16 15:02:20 +0900

    [CHORE] DB설정

--- src/main/java/com/nanuri/work/com/config/WebConfig.java
+++ src/main/java/com/nanuri/work/com/config/WebConfig.java
@@ -1,9 +1,25 @@
 package com.nanuri.work.com.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.nanuri.work.com.interceptor.LoggerInterceptor;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new LoggerInterceptor())
+				.excludePathPatterns("/css/**"
+									,"/fonts/**"
+									,"/img/**"
+									,"/js/**"
+									,"/error"
+									,"/static/*"
+									,"/favicon.ico"
+									); // Interceptor 예외
+	}
 
 }
