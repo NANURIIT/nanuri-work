9844f5195c3dd5012176b1766ea69256354058e0
SLCube <kgm0096@naver.com>
2022-04-04 09:55:17 +0900

    [REFACT] boardController와 boardApiController 분리

--- src/main/java/com/nanuri/work/business/board/controller/BoardController.java
+++ src/main/java/com/nanuri/work/business/board/controller/BoardController.java
@@ -1,21 +1,9 @@
 package com.nanuri.work.business.board.controller;
 
-import java.util.HashMap;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.nanuri.work.business.board.dto.BoardDTO;
-import com.nanuri.work.business.board.service.BoardService;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -23,9 +11,6 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 @RequestMapping(value = "/admin")
 public class BoardController {
-
-	@Autowired
-	private BoardService boardService;
 
 	@GetMapping(value = "/notice")
 	public String getNoticePage() {
@@ -40,44 +25,6 @@ public class BoardController {
 	@GetMapping(value = "noticeDetail")
 	public String getNoticeDetail() {
 		return "business/board/notice_detail";
-	}
-	
-	@ResponseBody
-	@PostMapping(value = "/boardWrite")
-	public String registerBoard(@RequestBody BoardDTO params) {
-		
-		String message = "";
-
-		try {
-			boolean isRegistered = boardService.registerBoard(params);
-			if (isRegistered == false) {
-				message = "등록에 실패하였습니다.";
-			}
-		} catch (Exception e) {
-			message = "시스템에 문제가 발생하였습니다.";
-			e.printStackTrace();
-		}
-		
-		return message;
-	}
-	
-	@ResponseBody
-	@GetMapping(value = "/boardList")
-	public HashMap<String, Object> getBoardList(@ModelAttribute("params") BoardDTO params) {
-		log.debug("params : " + params);
-		return boardService.getBoardList(params);
-	}
-	
-	@ResponseBody
-	@GetMapping(value = "/boardDetail/{seqNo}")
-	public BoardDTO getBoardDetail(@PathVariable(value = "seqNo", required = true) Long seqNo) {
-		return boardService.getBoardDetail(seqNo);
-	}
-	
-	@ResponseBody
-	@DeleteMapping(value = "/boardDelete")
-	public boolean deleteBoard(@RequestBody BoardDTO params) {
-		return boardService.deleteBoard(params);
 	}
 
 }
