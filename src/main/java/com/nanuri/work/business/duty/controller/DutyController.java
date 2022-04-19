0c9fd6f5cf3e8e84043a69667edb4444cb40c117
kangkyoungmo <kgm0096@naver.com>
2022-04-19 17:24:29 +0900

    [ADD] 근태관리 코드 기본구조 생성

---
+++ src/main/java/com/nanuri/work/business/duty/controller/DutyController.java
@@ -0,0 +1,11 @@
+package com.nanuri.work.business.duty.controller;
+
+import org.springframework.stereotype.Controller;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class DutyController {
+
+}
